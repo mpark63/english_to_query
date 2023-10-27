d514e8b7775c8af28247d2b1217eede50656f52b
mpark63 <minspk13@gmail.com>
2023-10-27 10:34:46 -0400

    working prune and max_prune

--- genquery.py
+++ genquery.py
@@ -28,7 +28,13 @@ def parse_args() -> argparse.Namespace:
         "--prune", 
         type=int,
         help="Remove top __% most frequent words",
-        default=0,
+    )
+
+    parser.add_argument(
+        "-mp",
+        "--maxprune", 
+        type=int,
+        help="Remove up to __% words in sentence",
     )
 
     # TODO: something with lexicon and removing non-Adj/NPs 
@@ -54,33 +60,97 @@ def parse_args() -> argparse.Namespace:
 
     return parser.parse_args()
 
+class WordCounter:
+    """A chart for Earley's algorithm."""
+    
+    def __init__(self) -> None:
+        """Create a counter of words from `tokens`"""
+        self.tokens = []
+        self.count = Counter()
+
+    def add_tokens(self, tokens: List[str]) -> None:
+        self.tokens.extend(tokens)
+        self.count.update(tokens)
+
+    def top_k_words(self, percent: float): 
+        top_k = math.floor(len(self.count) * percent)
+        return [t[0] for t in self.count.most_common(top_k)]
+    
+def filter_top_k(sentence: str, top_k_words: list[str]): 
+    tokens = sentence.split()
+    for token in tokens: 
+        if token in top_k_words: 
+            tokens.remove(token)
+    return " ".join(tokens)
+
+def filter_top_k_with_max(sentence: str, top_k_words: list[str], max_percent: float): 
+    tokens = sentence.split()
+    max_removals = math.floor(len(tokens) * max_percent)
+    nth_frequent = []
+    # take note of how frequent 
+    for token in tokens: 
+        if token in top_k_words:
+            nth_frequent.append(top_k_words.index(token))
+        else: 
+            nth_frequent.append(-1)
+    # remove the few most frequent 
+    remove_whitelist = nth_frequent.copy()
+    remove_whitelist = list(filter(lambda x: x != -1, remove_whitelist))
+    remove_whitelist.sort()
+    remove_whitelist = remove_whitelist[:max_removals]
+    new_sentence = ""
+    for index, word in enumerate(tokens): 
+        if index < len(nth_frequent) and nth_frequent[index] in remove_whitelist:
+            pass
+        else: 
+            new_sentence += word + " "
+    return new_sentence
+    
+
+
 def main():
     # Parse the command-line arguments
     args = parse_args()
     logging.basicConfig(level=args.logging_level) 
-
-    if args.prune != 0: 
-        # do something 
-        pass
-    if args.lexicon != None: 
-        pass
-    if args.model != None: 
-        pass
-
-    grammar = Grammar(args.start_symbol, args.grammar)
+    sentences: list[str] = []
 
     with open(args.sentences) as f:
         for sentence in f.readlines():
             sentence = sentence.strip()
             if sentence != "":  # skip blank lines
                 # analyze the sentence
-                log.debug(f"Parsing sentence: {sentence}")
-                chart = EarleyChart(sentence.split(), grammar, progress=args.progress)
-                # print the result
-                print(
-                    f"'{sentence}' is {'accepted' if chart.accepted() else 'rejected'} by {args.grammar}"
-                )
-                log.debug(f"Profile of work done: {chart.profile}")
+                log.debug(f"Reading sentence: {sentence}")
+                sentences.append(sentence)
+
+    if args.prune != None and args.prune != None: 
+        # create counter of words  
+        wc = WordCounter()
+        # add words from sentences
+        for sentence in sentences: 
+            wc.add_tokens(sentence.split())
+
+        # find top 10% of words
+        k_percent = args.prune / 100
+        top_k = wc.top_k_words(k_percent)
+        
+        # remove from sentences
+        for index, sentence in enumerate(sentences): 
+            sentences.remove(sentence)
+            sentence = sentence.strip()
+            log.debug(f"Modifying sentence: {sentence}")
+            if args.maxprune != None: 
+                res = filter_top_k_with_max(sentence, top_k, args.maxprune / 100)
+            else: 
+                res = filter_top_k(sentence, top_k)
+            sentences.insert(index, res)
+
+    if args.lexicon != None: 
+        pass
+    if args.model != None: 
+        pass
+    
+    for sentence in sentences: 
+        print(sentence)
 
 
 if __name__ == "__main__":
